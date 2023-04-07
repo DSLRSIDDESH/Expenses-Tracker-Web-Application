96d93ffa3f967e8241e1107acf9b77c780eb4758
Siddesh Devarakonda <siddeshdslr@gmail.com>
2023-04-07 22:49:35 +0530

    Added Income and Expenses in Home Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 export default function Navbar() {
+
+    const [showNavbar, setShowNavbar] = React.useState(false);
+    function handleShowNavbar() {setShowNavbar(prevShow => !prevShow);}
+
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+
+    React.useEffect(() => {
+        function watchWidth() {setWindowWidth(window.innerWidth)}
+        window.addEventListener("resize", watchWidth)
+        return function() {window.removeEventListener("resize", watchWidth)}
+    }, [])
+
     return (
         <nav className="nav">
             <Link to="/" className='navbar-brand'>PocketPilot</Link>
-            <ul>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/Events">Events</NavLink>
-                <NavLink to="/Stats">Stats</NavLink>
-                <NavLink to="/Profile">Profile</NavLink>
-            </ul>
+            { windowWidth < 660 &&
+                <div className='menu-icon menu-btn' onClick={handleShowNavbar}>
+                    <div class="menu-btn-burger"></div>
+                </div>
+            }
+            <div className={`nav-elements  ${showNavbar && 'ham-active'}`}>
+                <ul>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/Events">Events</NavLink>
+                    <NavLink to="/Stats">Stats</NavLink>
+                    <NavLink to="/Profile">Profile</NavLink>
+                </ul>
+            </div>
         </nav>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,104 @@
 import React from "react";
 
 export default function Home() {
+    const user_name = "John Doe"
+
+    const [balance, setBalance] = React.useState({
+        totalBalance: 0.00,
+        totalIncome: 0.00,
+        totalExpense: 0.00
+    })
+
+    const [addIncome, setAddIncome] = React.useState({show: false, value: 0.00})
+    const [addExpense, setAddExpense] = React.useState({show: false, value: 0.00})
+
+    function handleAddIncome() {
+        setAddIncome(prevAdd => {return {...prevAdd,show: !prevAdd.show}})
+        setAddExpense(prevAdd => {return {...prevAdd,show: false}})
+    }
+    function handleAddExpense() {
+        setAddExpense(prevAdd => {return {...prevAdd,show: !prevAdd.show}})
+        setAddIncome(prevAdd => {return {...prevAdd,show: false}})
+    }
+    function handleIncomeValue(e) {
+        const inputValue = e.target.value
+        if (/^\d*\.?\d*$/.test(inputValue)) {
+        setAddIncome(prevAdd => {return {...prevAdd,value: e.target.value}})
+        }
+    }
+    function handleIncomeSubmit(event) {
+        event.preventDefault()
+        setBalance(prevBal => {return {...prevBal,totalIncome: prevBal.totalIncome + parseFloat(addIncome.value)}})
+        setAddIncome(prevAdd => { return {...prevAdd,show: false, value: 0.00}})
+    }
+    function handleExpenseValue(e) {
+        const inputValue = e.target.value
+        if (/^\d*\.?\d*$/.test(inputValue)) {
+        setAddExpense(prevAdd => {return {...prevAdd,value: e.target.value}})
+        }
+    }
+    function handleExpenseSubmit(event) {
+        event.preventDefault()
+        setBalance(prevBal => {return {...prevBal,totalExpense: prevBal.totalExpense + parseFloat(addExpense.value)}})
+        setAddExpense(prevAdd => { return {...prevAdd,show: false, value: 0.00}})
+    }
+
+    React.useEffect(function() {
+        const income = balance.totalIncome
+        const expense = balance.totalExpense
+        setBalance(prevBal => {return {...prevBal, totalBalance: income - expense}})
+    },[balance.totalIncome, balance.totalExpense])
+
+    const handleKeyPress = (event) => {
+        const keyCode = event.keyCode || event.which;
+        const keyValue = String.fromCharCode(keyCode);
+        const regex = /^[0-9.]+$/;
+    
+        if (!regex.test(keyValue)) {
+          event.preventDefault();
+        }
+      };
+
+    const balanceType = balance.totalBalance === 0.00 ? "" : balance.totalBalance > 0.00 ? "+" : "-"
+
     return (
         <div>
-            <h1>Home</h1>
+            <p>Hello, {user_name}!</p>
+            <div>
+                <p>Total Balance</p>
+                <p>{balanceType}₹{Math.abs(balance.totalBalance)}</p>
+
+                <p>Incoming</p>
+                <p>+₹{balance.totalIncome}</p>
+                { !addIncome.show && <button onClick={handleAddIncome}>Add</button>}
+                {   
+                    addIncome.show &&
+                    <form onSubmit={handleIncomeSubmit}>
+                        <input 
+                            placeholder="Enter Amount" 
+                            onKeyDown={handleKeyPress}
+                            onChange={handleIncomeValue}
+                        />
+                        <button>Update</button>
+                    </form>
+                    
+                }
+
+                <p>Outgoing</p>
+                <p>-₹{balance.totalExpense}</p>
+                { !addExpense.show && <button onClick={handleAddExpense}>Add</button>}
+                {   
+                    addExpense.show &&
+                    <form onSubmit={handleExpenseSubmit}>
+                        <input 
+                            placeholder="Enter Amount" 
+                            onKeyDown={handleAddExpense}
+                            onChange={handleExpenseValue}
+                        />
+                        <button>Update</button>
+                    </form>
+                }
+            </div>
         </div>
     )
 }
