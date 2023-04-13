795fe66b1e7494cfba602eccf278ed0ef40cb0bb
Kartheek <mathikartheek79@gmail.com>
2023-04-13 22:40:55 +0530

    loginpage

---
+++ src/pages/upi.js
@@ -0,0 +1,38 @@
+import QRCode from 'qrcode'
+import { useState } from 'react'
+
+function App() {
+	const [url, setUrl] = useState('')
+	const [qr, setQr] = useState('')
+    const upiid='sbi@123.in'
+	const GenerateQRCode = () => {
+		QRCode.toDataURL(url, {
+			width: 150,
+			margin: 1,
+			color: {
+				dark: '#335383FF',
+				light: '#EEEEEEFF'
+			}
+		}, (err, url) => {
+			if (err) return console.error(err)
+
+			console.log(url)
+			setQr(url)
+		})
+	}
+
+	return (
+		<div className="app">
+			<input 
+				type="text"
+				value={url}
+				onChange={e => setUrl(e.target.value)} />
+			<button onClick={GenerateQRCode}>Generate</button>
+			{qr && <>
+				<img src={qr} />
+			</>}
+		</div>
+	)
+}
+
+export default App
